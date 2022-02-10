4a68bf207d94a28b91b455cf9b799ee8f0a52ccc
Hyeongyeong Kim <vetibeti@naver.com>
2022-02-10 10:26:37 +0900

    second commit_webscraping

--- webscraping_basic/9_bs4_coupang.py
+++ webscraping_basic/9_bs4_coupang.py
@@ -14,19 +14,40 @@ items = soup.find_all("li", attrs={"class":re.compile("^search-product")})
 #li 태그 중에서 class 정보가 search-product로 시작하는 모든 li element를 가져옴
 #print(items[0].find("div", attrs={"class":"name"}).get_text())
 for item in items:
+
+    # 광고 제품은 제외
+    ad_badge = item.find("spam" , attrs={"class":"ad-badge-text"})
+    if ad_badge:
+        print(" <광고 상품 제외합니다> ")
+        continue
+
     name = item.find("div", attrs={"class":"name"}).get_text() # 제품명
+
+    # 애플 제품 제외
+    if "Apple" in name:
+        print(" <Apple 상품 제외합니다> ")
+        continue
+
     price = item.find("strong", attrs={"class":"price-value"}).get_text() #가격
     
+    # 리뷰 100개 이상, 평점 4.5 이상 되는 것만 조회
     rate = item.find("em", attrs={"class":"rating"}) #평점이 있을수도 없을수도 있는 제품을 위해
     if rate:
         rate = rate.get_text()
     else:
-        rate = "평점 없음"
+       # rate = "평점 없음"
+        print(" <평점 없는 상품 제외합니다> ")
+        continue
    
     rate_cnt = item.find("span", attrs={"class":"rating-total-count"}) #평점 수
     if rate_cnt:
-        rate_cnt = rate_cnt.get_text()
+        rate_cnt = rate_cnt.get_text() # 예 : (26)
+        rate_cnt = rate_cnt[1:-1]
+        #print("리뷰 수", rate_cnt)
     else:
-        rate_cnt = "평점 수 없음"
+        # rate_cnt = "평점 수 없음"
+        print(" <평점 없는 상품 제외합니다> ")
+        continue
 
-    print(name, price, rate, rate_cnt)
+    if float(rate) >= 4.5 and int(rate_cnt) >= 100:
+       print(name, price, rate, rate_cnt)
